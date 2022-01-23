73d093da0260ce0ebcf641d1005676b6c17bfd90
Thiago Yamanaka <thiagoyb@gmail.com>
2022-01-23 17:35:57 -0300

    Convertido criação do servidor com Express + uso do nodemon

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res)=>{
+let app = express();
+
+app.get('/',(req, res)=>{
     console.log('URL: ', req.url);
     console.log('METHOD: ', req.method);
 
-    switch(req.url){
-        case '/':{
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Bem-vindo ao Node!<h1>');
-            break;
-        }
-        case '/users':{
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                'name' : 'Fulano Sauro',
-                'idade' : 22
-            }));
-            break;
-        }
-    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Bem-vindo!<h1>');
+});
+app.get('/users', (req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        'name' : 'Fulano Sauro',
+        'idade' : 29,
+        id: 1
+    });
 });
 
-server.listen(3000, '127.0.0.1', ()=>{
+app.listen(3000, '127.0.0.1', ()=>{
     console.log('Servidor iniciado na porta 3000!');
 });
