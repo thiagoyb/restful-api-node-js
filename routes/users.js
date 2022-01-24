ba013178a96d628ee8e5b69dcdd51baa04e2b731
Thiago Yamanaka <thiagoyb@gmail.com>
2022-01-23 22:14:00 -0300

    Buscando dados de um unico usuario

--- routes/users.js
+++ routes/users.js
@@ -31,4 +31,17 @@ module.exports = app =>{
             }
         });
     });
+
+    let routeId = app.route('/users/:id');
+    routeId.get((req,res)=>{
+        res.setHeader('Content-Type', 'application/json');
+
+        db.findOne({_id:req.params.id}).exec((err, user)=>{
+            if(err){
+                app.utils.error.send(err, req, res);
+            } else{
+                res.status(200).json(user);
+            }
+        });
+    });
 }
